a1ff4b8aead9a09805215b54b3767afbcc0b7811
adrian <adrian657984@gmail.com>
2023-10-19 07:59:31 +0800

    Add CrytographyService

---
+++ UrlConverter.NUnitTest/CrytographyServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlConverter.Service;
+
+namespace UrlConverter.NUnitTest
+{
+    public class CrytographyServiceTests
+    {
+        [TestCase("http://example.co/ysrAXmrmzPmkug2Z", 5)]
+        [TestCase("http://example.co/ysrAXmrmzPmkug2Z", 12)]
+        public void EncryptUrl_LengthInRangeTest(string originalUrl, int maxLength)
+        {   
+            string encryptedUrl = CrytographyService.EncryptUrl(originalUrl, maxLength);
+            bool isLengthInRange = encryptedUrl.Length <= maxLength;
+
+            Assert.IsTrue(isLengthInRange);
+        }
+    }
+}
