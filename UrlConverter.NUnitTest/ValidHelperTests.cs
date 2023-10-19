a2c75dd60a2d243c6b788806fb63d42354624daa
adrian <adrian657984@gmail.com>
2023-10-19 08:00:27 +0800

    Add ValidHelper

---
+++ UrlConverter.NUnitTest/ValidHelperTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlConverter.Helper;
+
+namespace UrlConverter.NUnitTest
+{
+    public class ValidHelperTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [TestCase("https://example.co/ysrAXmrmzPmkug2Z")]
+        [TestCase("http://example.co/ysrAXmrmzPmkug2Z")]
+        public void IsUrlValid_TrueTest(string url)
+        {
+            var validHelper = new ValidHelper();
+            bool isValid = validHelper.IsUrlValid(url);
+
+            Assert.IsTrue(isValid);
+        }
+
+        [TestCase("htt://example.co/ysrAXmrmzPmkug2Z")]
+        [TestCase("httpp://example.co/ysrAXmrmzPmkug2Z")]
+        public void IsUrlValid_FalseTest(string url)
+        {
+            var validHelper = new ValidHelper();
+            bool isValid = validHelper.IsUrlValid(url);
+
+            Assert.IsFalse(isValid);
+        }
+    }
+}
---
+++ UrlConverter/Helper/ValidHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace UrlConverter.Helper
+{
+    public class ValidHelper
+    {
+        private readonly HashSet<string> formats = new HashSet<string> { "http://", "https://" };
+        public bool IsUrlValid(string url)
+        {
+            return formats.Any(fmt => url.StartsWith(fmt));
+        }
+    }
+}
